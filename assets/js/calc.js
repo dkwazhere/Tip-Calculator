58c9d2f670321a9d21693ccb5febd557ea836c67
dkwazhere <dhealth77@gmail.com>
2018-09-29 07:32:39 -0700

    added js, calculateTip function, and validator

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,3 +1,24 @@
+// Custom function
+function calculateTip() {
+  // Store the data of inputs
+  var billAmount = document.getElementById("billAmount").value;
+  var serviceQuality = document.getElementById("serviceQuality").value;
+  var numPeople = document.getElementById("totalPeople").value;
+
+  console.log(billAmount + " " + serviceQuality + " " + numPeople);
+
+  // Quick Validation
+  if (billAmount === "" || serviceQuality == 0) {
+    window.alert("please enter some values");
+    return;
+  }
+}
+
 // Hide the tip amount
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
+
+// Clicking the button calls our custom function
+document.getElementById("calculator").onclick = function() {
+  calculateTip();
+};
