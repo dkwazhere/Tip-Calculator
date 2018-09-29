e7bb6859e13ad3098dac7325ce65275c6a6acc1c
dkwazhere <dhealth77@gmail.com>
2018-09-29 07:48:32 -0700

    finished calculator javascript end

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -9,9 +9,29 @@ function calculateTip() {
 
   // Quick Validation
   if (billAmount === "" || serviceQuality == 0) {
-    window.alert("please enter some values");
-    return;
+    // window.alert("please enter some values");
+    return; // This will prevent the funciton from continuing
   }
+
+  // Check to see if this input is empty or less than or equal to 1.
+
+  if (numPeople == "" || numPeople <= 1) {
+    numPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  // Math
+  var total = (billAmount * serviceQuality) / numPeople;
+  // rounding decimal places
+  total = Math.round(total * 100) / 100;
+  // Make sure decimal places has two digits
+  total = total.toFixed(2);
+
+  // Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
 }
 
 // Hide the tip amount
